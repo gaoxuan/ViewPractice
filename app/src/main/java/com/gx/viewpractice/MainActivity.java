a30213b9d46d6efb333b617e520ff4a8ecaa8ff0
gaoxuan <gaoxuan44@gmail.com>
2017-08-16 18:17:11 +0800

    初始化工程

---
+++ app/src/main/java/com/gx/viewpractice/MainActivity.java
@@ -0,0 +1,21 @@
+package com.gx.viewpractice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.lv_main)
+    ListView lvMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+}
---
+++ app/src/main/java/com/gx/viewpractice/view/Session1.java
@@ -0,0 +1,8 @@
+package com.gx.viewpractice.view;
+
+/**
+ * Created by gaoxuan on 2017/8/14.
+ */
+
+public class Session1 {
+}
