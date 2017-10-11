1aa034aa40f2aeb6f849276a50839cfeff9a1837
gaoxuan <gaoxuan44@gmail.com>
2017-10-12 00:24:37 +0800

    更新园形分离算法

--- app/src/main/java/com/gx/viewpractice/view/Session1.java
+++ app/src/main/java/com/gx/viewpractice/view/Session1.java
@@ -44,7 +44,6 @@ public class Session1 extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         int screenWidth = ScreenUtils.getScreenWidth(mContext);
-        int screenHeight = ScreenUtils.getScreenHeight(mContext);
 
         Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setColor(Color.RED);
@@ -54,65 +53,71 @@ public class Session1 extends View {
         int itemW = screenWidth / 4;
         RectF originalRectF = new RectF(itemW, itemW, itemW * 3, itemW * 3);
         double offsetCenter = 20; // 新扇形偏离圆心距离
-        Log.i("calcRectF", "original-left:" + originalRectF.left + ", right:" + originalRectF.right +
-                ", top:" + originalRectF.top + ", bottom:" + originalRectF.bottom);
 
+        Paint linePaint = new Paint(Color.BLACK);
+        linePaint.setStyle(Paint.Style.STROKE);
+        canvas.drawRect(originalRectF, linePaint);
 
-//        RectF rectF = calcRectF(originalRectF, 120, offsetCenter);
-        RectF rectF = new RectF(originalRectF);
-        rectF.offset(-10, -17);
-        Log.i("calcRectF", "rectF-left:" + rectF.left + ", right:" + rectF.right +
-                ", top:" + rectF.top + ", bottom:" + rectF.bottom);
+        float cx = screenWidth / 2;
+        Paint circlePaint = new Paint();
+        circlePaint.setColor(Color.BLUE);
+        canvas.drawCircle(cx, cx, 20, circlePaint);
+
+        RectF rectF = calcRectF(originalRectF, -180, 120, offsetCenter);
         canvas.drawArc(rectF, -180, 120, true, paint);
+        linePaint.setColor(Color.RED);
+        canvas.drawRect(rectF, linePaint);
 
         paint.setColor(Color.YELLOW);
-//        RectF rectF1 = calcRectF(originalRectF, 55, offsetCenter);
-        RectF rectF1 = new RectF(originalRectF);
-        rectF1.offset(17, -9);
-        Log.i("calcRectF", "rectF1-left:" + rectF1.left + ", right:" + rectF1.right +
-                ", top:" + rectF1.top + ", bottom:" + rectF1.bottom);
+        RectF rectF1 = calcRectF(originalRectF, -60, 55, offsetCenter);
         canvas.drawArc(rectF1, -60, 55, true, paint);
+        linePaint.setColor(Color.YELLOW);
+        canvas.drawRect(rectF1, linePaint);
 
         paint.setColor(Color.GREEN);
-//        RectF rectF2 = calcRectF(originalRectF, 15, offsetCenter);
-        RectF rectF2 = new RectF(originalRectF);
-        rectF2.offset(19, 2);
-        Log.i("calcRectF", "rectF2-left:" + rectF2.left + ", right:" + rectF2.right +
-                ", top:" + rectF2.top + ", bottom:" + rectF2.bottom);
+        RectF rectF2 = calcRectF(originalRectF, -5, 15, offsetCenter);
         canvas.drawArc(rectF2, -5, 15, true, paint);
+        linePaint.setColor(Color.GREEN);
+        canvas.drawRect(rectF2, linePaint);
 
         paint.setColor(Color.CYAN);
-//        RectF rectF3 = calcRectF(originalRectF, 10, offsetCenter);
-        RectF rectF3 = new RectF(originalRectF);
-        rectF3.offset(19, 1);
-        Log.i("calcRectF", "rectF3-left:" + rectF3.left + ", right:" + rectF3.right +
-                ", top:" + rectF3.top + ", bottom:" + rectF3.bottom);
+        RectF rectF3 = calcRectF(originalRectF, 10, 10, offsetCenter);
         canvas.drawArc(rectF3, 10, 10, true, paint);
+        canvas.drawRect(rectF3, linePaint);
 
         paint.setColor(Color.MAGENTA);
-//        RectF rectF4 = calcRectF(originalRectF, 45, offsetCenter);
-        RectF rectF4 = new RectF(originalRectF);
-        rectF4.offset(18, 7);
-        Log.i("calcRectF", "rectF4-left:" + rectF4.left + ", right:" + rectF4.right +
-                ", top:" + rectF4.top + ", bottom:" + rectF4.bottom);
+        RectF rectF4 = calcRectF(originalRectF, 20, 45, offsetCenter);
         canvas.drawArc(rectF4, 20, 45, true, paint);
+        linePaint.setColor(Color.MAGENTA);
+        canvas.drawRect(rectF4, linePaint);
 
         paint.setColor(Color.LTGRAY);
-//        RectF rectF5 = calcRectF(originalRectF, 115, offsetCenter);
-        RectF rectF5 = new RectF(originalRectF);
-        rectF5.offset(-10, 16);
-        Log.i("calcRectF", "rectF4-left:" + rectF5.left + ", right:" + rectF5.right +
-                ", top:" + rectF5.top + ", bottom:" + rectF5.bottom);
+        RectF rectF5 = calcRectF(originalRectF, 65, 115, offsetCenter);
         canvas.drawArc(rectF5, 65, 115, true, paint);
+        linePaint.setColor(Color.LTGRAY);
+        canvas.drawRect(rectF5, linePaint);
+
     }
 
-    private RectF calcRectF(RectF originalRectF, double angle, double offset) {
+    private RectF calcRectF(RectF originalRectF, double startAngle, double deltaAngle, double offset) {
+        // actAngle = |deltaAngle| / 2 + actStartAngle
+
+        double actAngle;
+        double actStartAngle;
+        if (startAngle < 0) {
+            actStartAngle = -startAngle - deltaAngle;
+            actAngle = actStartAngle + deltaAngle / 2;
+        } else {
+            actStartAngle = -startAngle;
+            actAngle = actStartAngle - deltaAngle / 2;
+        }
+
         RectF rectF = new RectF(originalRectF);
-        double actAngle = Math.toRadians((360 - angle) / 2);
-        float deltaX = new BigDecimal(offset).multiply(new BigDecimal(Math.cos(actAngle))).floatValue();
-        float deltaY = new BigDecimal(offset).multiply(new BigDecimal(Math.sin(actAngle))).floatValue();
-        rectF.offset(deltaX, deltaY);
-        Log.i("calcRectF", "deltaX:" + deltaX + ", deltaY:" + deltaY);
+        double formatAngle = Math.toRadians(actAngle);
+        float deltaX = (float) (offset * Math.cos(formatAngle));
+        float deltaY = (float) (offset * Math.sin(formatAngle));
+
+        rectF.offset(deltaX, -deltaY);
 
         return rectF;
     }
